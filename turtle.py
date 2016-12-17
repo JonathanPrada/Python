9c7a6b7619fd99210ab7a06fad4f4ea92da202ee
Jonathan <jonathanprada@protonmail.ch>
2016-12-17 14:13:13 +0100

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,43 +1,29 @@
 import turtle
 
-start = 50
-number = 100
+def draw_square(some_turtle):
+    for i in range (1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
-#Window settings
-def window():
-    window = turtle.Screen()
-    window.bgcolor("white")
-
-#Draw lines           
-def draw_lines(some_turtle):
-
-        global start
-        some_turtle.forward(start)
-        some_turtle.right(90)
-        some_turtle.forward(number)
-        some_turtle.right(90)
-        some_turtle.forward(number)
-        some_turtle.right(90)
-        some_turtle.forward(number)
-        some_turtle.right(90)
-        global number
-        number = number + (number*2)
-        start = start + 20
-        
 def draw_art():
-
-    #Create the window
-    window()
+    
+    window = turtle.Screen()
+    window.bgcolor("red")
     #Create the turtle brad - draw square
     brad = turtle.Turtle()
     brad.shape("turtle")
-    brad.color("black")
-    brad.speed(4)
-    for i in range (1,5):
-        draw_lines(brad)
- 
+    brad.color("yellow")
+    brad.speed(100)
+    #calls the draw_square function
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+    #create the turtle angie - draw circle
+    angie = turtle.Turtle()
+    angie.color("blue")
+    angie.shape("turtle")
+    angie.circle(100) 
+
     window.exitonclick()
     
 draw_art()
-
-
