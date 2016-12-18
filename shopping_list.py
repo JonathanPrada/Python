953b12d53cdb73f91aeb5eb6037aef450b482f91
Jonathan <jonathanprada@protonmail.ch>
2016-12-18 14:34:28 +0100

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,24 +1,50 @@
+#TODO: Need to add delete option
+#TODO: Need to add send functionality
+#TODO: Need to validate input
+
 #make a list to hold onto our items
 shopping_list = []
 
 #print out instructions on how to use the app
-print("What should we pick up at the store?")
-print("Enter 'DONE' to stop adding items.")
+def show_help():
+  print("What should we pick up at the store?")
+  print("""
+Enter 'DONE' to stop adding to the list.
+Enter 'HELP' to show this again.
+Enter 'SHOW' to show the shopping list.
+""")
+
+#Show the items in the list
+def show_list():
+    print("Here are the current items listed: ")
+    for item in shopping_list:
+      print(item)
+      
+#add new items to our list
+def add_item():
+    shopping_list.append(new_item)
+    print("Added {}. The list has now {} items.".format(new_item, len(shopping_list)))
+
+show_help()
 
 while True:
   #ask for new items
   new_item = input("> ")
-  
+
   #be able to quit the app
   if new_item == 'DONE':
     break
-  
-  #add new items to our list
-  shopping_list.append(new_item)
+  elif new_item == 'HELP':
+    show_help()
+    continue
+  elif new_item == 'SHOW':
+    show_list()
+    continue
+  else:
+    add_item()
 
 #print out the list
 print("Here's your list:")
-
 for item in shopping_list:
     print(item)
   
